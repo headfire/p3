400bc0f0309c160df50d16c044039358a8028d13
headfire <headfire@yandex.ru>
2020-09-08 13:55:49 +0300

    Dao presentation title correct

--- viewer/js/main.js
+++ viewer/js/main.js
@@ -128,7 +128,7 @@ function mainLoadSlideInfo(paper, slide) {
      if (xmlhttp.readyState == 4) {
        if (xmlhttp.status == 200) {
 	       var slideInfo = JSON.parse(xmlhttp.responseText);
-		   var slideName = 'Точка сборки - 3D. ' + slideInfo.slideName;
+		   var slideName = '<font color=gray>Точка сборки - 3D</font>&nbsp;&nbsp;&nbsp;&nbsp;' + slideInfo.slideName;
 		   mainSetHtmlByClass('slide-name', slideName)
 	    }
 	 }
