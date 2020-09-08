f2cd8dfe2d6783ec7355577fe31c3de904038d93
headfire <headfire@yandex.ru>
2020-09-08 14:00:50 +0300

    correct dao

--- viewer/slides/dao/slide_03_DaoSecPrincipe/slide.js
+++ viewer/slides/dao/slide_03_DaoSecPrincipe/slide.js
@@ -14,7 +14,6 @@ function loadedSlideMake(slidePath) {
 	zdeskXCurve(slidePath+'exp_001_edge.json', 0xcccccc, 1);
 	zdeskXCurve(slidePath+'exp_002_wire.json', 0x1919e5, 4);
 	zdeskXPoint(0, -10, 0, 0x1919e5, 6);
-	zdeskXLabel(0, -10, 0, 'f0', 0x1919e5);
 	zdeskXCurve(slidePath+'exp_003_edge.json', 0xe51919, 2);
 	zdeskXShape(slidePath+'exp_004_shape.json', 0xe51919, 0x333333, 0.9, 0.3);
 	zdeskXPoint(33.7486, 15.1669, 0, 0xe51919, 6);
