529709415523e4a88d996d4111f6de1b56d8ee26
Headfire <headfire@yandex.ru>
2022-04-03 08:18:48 +0300

    fix slide dao 03

--- slides/dao_03/slide.js
+++ slides/dao_03/slide.js
@@ -12,10 +12,10 @@ function loadedSlideGetParam() {
 
 function loadedSlideMake() { 
 	zdeskXCurve('exp_001_edge', 0x4c4c4c, 2);
-	zdeskXCurve(slidePath+'exp_002_wire', 0x1919e5, 4);
+	zdeskXCurve('exp_002_wire', 0x1919e5, 4);
 	zdeskXPoint(0, -10, 0, 0x1919e5, 3);
-	zdeskXCurve('exp_003_edge.json', 0xe51919, 2);
-	zdeskXShape('exp_004_shape.json', 0xe51919, 0x333333, 0.9, 0.3);
+	zdeskXCurve('exp_003_edge', 0xe51919, 2);
+	zdeskXShape('exp_004_shape', 0xe51919, 0x333333, 0.9, 0.3);
 	zdeskXPoint(33.7486, 15.1669, 0, 0xe51919, 3);
 	zdeskXPoint(12.4894, -0.686428, 0, 0xe51919, 3);
 	zdeskXCurve('exp_005_wire', 0xe51919, 2);
