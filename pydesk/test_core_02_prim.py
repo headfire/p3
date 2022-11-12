97d02d2ecd79adb1d71fe7279624a3131c519bf7
headfire <headfire@yandex.ru>
2022-11-13 00:35:22 +0300

    Full prim test

--- pydesk/test_core_02_prim.py
+++ pydesk/test_core_02_prim.py
@@ -5,7 +5,30 @@ from core_draw import Pnt, LineDraw #, VectorDraw
 screen = ScreenRenderLib()
 screen.renderStart()
 
-screen.render(LineDraw(Pnt(0, 0, 0), Pnt(200, 200, 200)))
+pnt000 = Pnt(0, 0, 0)
+pnt001 = Pnt(0, 0, 100)
+pnt010 = Pnt(0, 100, 0)
+pnt011 = Pnt(0, 100, 100)
+pnt100 = Pnt(100, 0, 0)
+pnt101 = Pnt(100, 0, 100)
+pnt110 = Pnt(100, 100, 0)
+pnt111 = Pnt(100, 100, 100)
+
+screen.render(LineDraw(pnt000,pnt001))
+screen.render(LineDraw(pnt001,pnt011))
+screen.render(LineDraw(pnt011,pnt010))
+screen.render(LineDraw(pnt010,pnt000))
+
+screen.render(LineDraw(pnt100,pnt101))
+screen.render(LineDraw(pnt101,pnt111))
+screen.render(LineDraw(pnt111,pnt110))
+screen.render(LineDraw(pnt110,pnt100))
+
+screen.render(LineDraw(pnt000,pnt100))
+screen.render(LineDraw(pnt001,pnt101))
+screen.render(LineDraw(pnt010,pnt110))
+screen.render(LineDraw(pnt011,pnt111))
+
 # screen.render(VectorDraw(Pnt(0, 0, 0), Pnt(200, 0, 0)))
 
 screen.renderFinish()
