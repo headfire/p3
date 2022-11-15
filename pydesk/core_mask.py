3d7bbf91b83f037ca01a7b5fd5e70ab79f4b56a4
Michail <headfire@yandex.ru>
2022-11-15 10:37:55 +0300

    Mask ok

--- pydesk/core_mask.py
+++ pydesk/core_mask.py
@@ -1,4 +1,4 @@
-def isGoodName(nameMask: str, nameValue: str):
+def isNameMask(nameMask: str, nameValue: str):
     if nameMask.endswith('*'):
         return nameValue.startswith(nameMask.replace('*', ''))
     else:
@@ -7,12 +7,14 @@ def isGoodName(nameMask: str, nameValue: str):
 
 def isClassMask(classMask, classValues):
     for classValue in classValues.split('-'):
-        if classValue == classMask
+        if classValue == classMask:
             return True
     return False
 
 
 def isClassesMask(classesMask: str, classesValue: str):
+    if classesMask == '*':
+        return True
     for classMask in classesMask.split('-'):
         if not isClassMask(classMask, classesValue):
             return False
@@ -20,7 +22,6 @@ def isClassesMask(classesMask: str, classesValue: str):
 
 
 def isObjMask(objMask, objValue):
-
     objMaskList = objMask.split(':')
     nameMask = '*'
     if len(objMaskList) > 0:
@@ -36,29 +37,47 @@ def isObjMask(objMask, objValue):
     classesValue = ''
     if len(objValueList) > 1:
         classesValue = objValueList[1]
+    return isNameMask(nameMask, nameValue) and isClassesMask(classesMask, classesValue)
 
-    return isGoodName(nameMask, nameValue) and isGoodClasses(classesMask, classesValue)
 
-
-def isMask(objChainMask, objChainValue)
-    objChainMaskList =  objChainMask.split('>')
-    objChainValueList =  objChainValue.split('>')
+def isMask(objChainMask, objChainValue):
+    objChainMaskList = objChainMask.split('>')
+    objChainValueList = objChainValue.split('>')
     iMask, iValue = 0, 0
-    while (iMask<len(objChainMaskList)) and (iValue<len(objChainMaskList)):
-        if isObjMask(objChainMaskList[i],objChainValueList):
+    while (iMask < len(objChainMaskList)) and (iValue < len(objChainValueList)):
+        if isObjMask(objChainMaskList[iMask], objChainValueList[iValue]):
             iMask += 1
             iValue += 1
         else:
             iValue += 1
-    return  (iMask == len(objChainMaskList)) and iValue == len(objChainMaskList)
+    return (iMask == len(objChainMaskList)) and iValue == len(objChainValueList)
 
 
-def check(mask, value, result)
-    if isMask(mask, value) == result:
+def check(mask, value, expected):
+    result = isMask(mask, value)
+    if  result == expected:
         print('Test OK', mask, value, result)
     else:
-        print('Test ERROR', mask, value, result)
+        print('Test ERROR', mask, value, expected, 'RESULT', result)
+
 
 if __name__ == "__main__":
 
-   check('item','item', True)
+    check('item', 'item', True)
+    check('item', 'item200', False)
+    check('*', 'item', True)
+    check('item*', 'item200', True)
+    check('item3*', 'item200', False)
+    check('item', 'item:box', True)
+    check('item:box', 'item:box', True)
+    check('item:line', 'item:box', False)
+    check('item:line-box', 'item:box', False)
+    check('item:box', 'item:line-box', True)
+    check('item:line-box', 'item:line-box', True)
+    check('item:line-box-info', 'item:line-box', False)
+    check('item:line-box-info', 'item:info-line-box', True)
+    check('i*:line-box-info', 'item:info-line-box', True)
+    check('item2>item', 'item2>item', True)
+    check('item2>item', 'item2>item0>item', True)
+    check('item3>item2>item', 'item000>item3>item2>item0>item00>item', True)
+    check('item3>item4>item', 'item000>item3>item2>item0>item00>item', False)
